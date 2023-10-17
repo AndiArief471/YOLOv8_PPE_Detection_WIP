b3b19890e57959578cbbdfc20cd20a8246e17e96
AndiArief471 <andi.arief471@gmail.com>
2023-10-17 16:38:11 +0800

    Update code

--- 16-10-2023_TestYolov8.py
+++ 16-10-2023_TestYolov8.py
@@ -1,8 +1,7 @@
 from ultralytics import YOLO
 import cv2
-from ultralytics.utils.plotting import Annotator  # ultralytics.yolo.utils.plotting is deprecated
+import numpy as np
 
-list = [200, 200, 400, 400]
 
 model = YOLO('yolov8n.pt')
 cap = cv2.VideoCapture(0)
@@ -12,21 +11,31 @@ cap.set(4, 720)
 while True:
     ret, frame = cap.read()
 
+    list = []
+    masks = []
+    total_mask = np.zeros(frame.shape[:2], dtype=np.uint8)
+    outside_mask = 255
+
     result = model.predict(frame, show=True, classes=0)
-
     for r in result:
-        boxes = r.boxes
-
-        x1 = int(r[0].boxes.xyxy[0][0])
-        y1 = int(r[0].boxes.xyxy[0][1])
-        x2 = int(r[0].boxes.xyxy[0][2])
-        y2 = int(r[0].boxes.xyxy[0][3])
-        print(f"{x1}, {y1}, {x2}, {y2}")
-
+        for i in range(len(r)):
+            list.append(np.array(r[i].boxes.xyxy[0]))
 
     if (cv2.waitKey(30) == 32):
-        cropped_frame = frame[y1:y2, x1:x2]
-        result2 = model.predict(cropped_frame, classes=67)
+        print("Spacebar Pressed!")
+        black_background = np.zeros_like(frame)
+        for i in range(len(list)):
+            print("Entering for loop")
+            mask = np.zeros(frame.shape[:2], dtype=np.uint8)
+            masks.append(mask)
+            cv2.rectangle(masks[i], (int(list[i][0]), int(list[i][1])), (int(list[i][2]), int(list[i][3])), 255, -1)
+            outside_mask -= masks[i]
+            total_mask += masks[i]
+        print(outside_mask)
+        print(total_mask)
+        result2 = cv2.add(cv2.bitwise_and(frame, frame, mask=total_mask),
+                          cv2.bitwise_and(black_background, black_background, mask=outside_mask))
+        cv2.imshow('Image with Shapes on Black Background', result2)
 
-    if (cv2.waitKey(30) == 27):
+    if (cv2.waitKey(1) == 27):
         break
